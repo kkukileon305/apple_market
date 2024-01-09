0ecda5e94d30a6d4c0204e12effb882bc92ee1d0
kkukileon305 <kkukileon305@gmail.com>
2024-01-09 20:45:57 +0900

    add detail binding

--- app/src/main/java/com/goodness/applemarket/ProductDetail.kt
+++ app/src/main/java/com/goodness/applemarket/ProductDetail.kt
@@ -3,6 +3,8 @@ package com.goodness.applemarket
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.goodness.applemarket.databinding.ActivityProductDetailBinding
+import java.text.NumberFormat
+import java.util.Locale
 
 class ProductDetail : AppCompatActivity() {
 	private val binding by lazy { ActivityProductDetailBinding.inflate(layoutInflater) }
@@ -11,8 +13,24 @@ class ProductDetail : AppCompatActivity() {
 		super.onCreate(savedInstanceState)
 		setContentView(binding.root)
 
-		intent.getParcelableExtra<Product>("Product")?.let {
-			binding.tvProductTitle.text = it.title
+		bindData()
+		setHandler()
+	}
+
+	private fun bindData() {
+		intent.getParcelableExtra<Product>("Product")?.let { product ->
+			binding.tvProductSeller.text = product.seller
+			binding.tvProductPlace.text = product.place
+			binding.ivProductImage.setImageResource(product.image)
+			binding.tvProductTitle.text = product.title
+			binding.tvProductDesc.text = product.desc
+			binding.tvProductPrice.text = "${NumberFormat.getNumberInstance(Locale.KOREA).format(product.price)}원"
+		}
+	}
+
+	private fun setHandler() {
+		binding.ivHome.setOnClickListener {
+			finish()
 		}
 	}
 }
